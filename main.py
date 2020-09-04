b687844c68d174a01e1722ddb079cc844c47117d
hamadazahera <hamadazahera@gmail.com>
2020-09-04 11:42:54 +0200

    clustering events and evaluate based on type predictions

--- main.py
+++ main.py
@@ -1,64 +1,91 @@
+import os
 import pandas as pd
 import numpy as np
 import hdbscan
-import time
 from clustering_evaluation import ClusterPurity
 
-#-- compute runtime at the beginning--#
-start_time = time.time()
-
-#path=
 def hdbscan_clustering(cluster_df):
     """
-    param: path of embeddings dataframe to be clustered
-    return: clustered dataframe
+    param: Embeddings dataframe to be clustered
+    return: a clustered dataframe
     """
 
     ##-- Cluster the data using HDBSCAN --### -- Consider Hyperparameter tuning later --#
     clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.3, approx_min_span_tree=True, metric='euclidean',
-     gen_min_span_tree=True, min_cluster_size=1000, min_samples=100, cluster_selection_method='leaf', allow_single_cluster=False).fit(cluster_df)
+     gen_min_span_tree=True, min_cluster_size=10000, min_samples=100, cluster_selection_epsilon= 0.5, 
+                                core_dist_n_jobs=1,allow_single_cluster=False).fit(cluster_df)
 
     #-- Save clustering results as a new column in the original dataframe --#
-    cluster_df['labels_preds']=clusterer.labels_
+    cluster_df['cluster_preds']=clusterer.labels_
     
-    print ('Number of Clusters',len(set(clusterer.labels_)))
+    print ('Number of Clusters: ',len(set(clusterer.labels_)))
 
     return cluster_df
 
 
-def evaluate_ClusteringPurity(cluster_df):
+def evaluate_ClusteringPurity(cluster_preds, cluster_true):
     """
-    param: cluster_df : clustered_dataframe
+    param: 
+        cluster_preds: predict cluster label
+        cluster_true: the ground truth labels of clusters. Here, we use 'type' column from 
+        the original dataset.
+        
     return purity_score
     """
-
-    labels_preds=cluster_df['labels_preds'].tolist()
-    labels_true=cluster_df['labels_true'].tolist()
 
     evaluator=ClusterPurity()
-    purity_score=evaluator.purity_score(y_true=labels_true, y_pred=labels_preds)
+    purity_score=evaluator.purity_score(y_true=cluster_true, y_pred=cluster_preds)
 
     return purity_score
 
 
+def cluster_distriubions(clustered_df):
+    
+    print('Clusters Distributions: ')
+    cluster_distributions=clustered_df.groupby('cluster_preds').size()
+    for cluster, size in cluster_distributions.items():
+        print (cluster, size)
+
+        
+        
 if __name__ == "__main__":
 
-    # -- load the clustering input --#
-    path='/home/hzahera/Documents/SVN/Vectograph-develop/Vectograph_Results/2020-07-11 14:35:17.987772/PYKE_50_embd.csv' # e.g. Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv
-    cluster_df=pd.read_csv(path, index_col=0)
+    input_data='/home/daikiri/DAIKIRI/src/Hamada/merged.csv' # use your own path
+    embedding_data='/home/daikiri/DAIKIRI/src/Hamada/Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv' 
 
-    #- In case of sampling the input data, uncomment this code 
-    cluster_df=cluster_df.sample(frac=0.3, replace=True, random_state=42)
+    # -- load the input dataset--#
+    input_df=pd.read_csv(input_data, low_memory=True)
+    input_df.index = 'Event_' + input_df.index.astype(str)
+    num_rows, num_cols = input_df.shape  # at max num_rows times num_cols columns.
+    column_names = input_df.columns
     
-    print ('Data loaded with shape', cluster_df.shape )
+    print ('input_data (ai4bd) loaded with shape', input_df.shape) 
+    
+    #-- load the embeddings data --#
+    embedding_input=pd.read_csv(embedding_data, index_col=0, low_memory=True)
+    #consider only events embedding (Event_id)
+    embedding_index=embedding_input.index.tolist()
+    prefix = 'Event_'
+    event_ids=list(filter(lambda x: x.startswith(prefix), embedding_index))
+    events_df=embedding_input.loc[event_ids]
+    
+    print ('embedding_data loaded with shape', events_df.shape )
 
-    # -- Add y_true as column with random values --#
-    cluster_df['labels_true']=np.random.randint(-1, 880, cluster_df.shape[0]) # generate random cluster numbers range from -1 to total number of clusters
+    #-- Clustering using HDBSCAN --#
+    clustered_df=hdbscan_clustering(events_df)
 
-    clustered_df=hdbscan_clustering(cluster_df)
+    cluster_preds= events_df['cluster_preds'].tolist()
+    cluster_true= input_df['type'].tolist()
 
-    purity_score=evaluate_ClusteringPurity(clustered_df)
-    print('Cluster Purity Score: ', purity_score)
+    #-- Evaluate the clustering performance --#
+    purity_score=evaluate_ClusteringPurity(cluster_preds, cluster_true)
+    print('Clustering Purity Score: ', purity_score)
+    
+    #-- Evaluate Cluster Validty --#
+    #- https://hdbscan.readthedocs.io/en/latest/api.html#hdbscan.validity.validity_index
+    print ('Clustering Validty Score: ',hdbscan.validity.validity_index(events_df, cluster_preds))
+    
+    # -- Print Cluster Distributions --#
+    cluster_distriubions(clustered_df)
 
-    print ('Done..')
-
+    
