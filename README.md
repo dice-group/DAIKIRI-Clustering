952f320bd657a4dd7f884af4088179831ec7b332
hamadazahera <hamadazahera@gmail.com>
2020-09-09 01:51:15 +0200

    add commandline arguments and updated readme

--- README.md
+++ README.md
@@ -1,32 +1,46 @@
 # DAIKIRI-Clustering
-* Create a first prototype for AP3.1 using HDBSCAN algorithm. 
-* The clustering performance is measure using cluster_purity metric. 
 
----
 ## Clustering Demo using HDBSCAN
+To run the clustering demo (in main.py) using hdbscan algorithm, type the following command line as follows:
+<font size="-2">
+```python
+python3 main --input_data '/home/daikiri/DAIKIRI/src/Hamada/merged.csv' --embedding_data '/home/daikiri/DAIKIRI/src/Hamada/Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv'  --config param.txt
+```
+</font>
 
-### Input Data:
-- The module takes two inputs file: original data (e.g `ai4bd.csv`).
-- The embeddings data generated from the previous module, it can be found fore example in `Vectograph_Results/../PYKE_50_embd.csv` 
+### CommandLine Arguments:
+- `input_data`: Path of input data e.g. ai4bd.csv or merged.csv
+- `embedding_data`: Path of embedding data e.g.Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv
+- `config`: Path of config file to read hyper-parameter values. e.g. param.txt
 
-The clustering_demo (in main.py) uses hdbscan with tuned hyperparameters to cluster the events data based on their embeddings. 
-### Tuning Hyperparameters
-- `min_cluster_size` is set to 10000 samples per cluster
-- `min_samples` is set to 100 to consider a core point in HDBSCAN
-- `cluster_selection_epsilon` is set to 0.5 to merge small clusters.
-
-Other parameters can be shown here:
+### Hyperparameters:
+hyper-parameter values:
+<font size="-2">
 ```python
 clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.3, approx_min_span_tree=True, metric='euclidean', gen_min_span_tree=True, min_cluster_size=10000, min_samples=100, cluster_selection_epsilon= 0.5, core_dist_n_jobs=1, allow_single_cluster=False).fit(cluster_df)
 ```
+</font>
+To try other hyper-parameters values for HDBSCAN, please use argument `--config params.txt`. An example in param.txt.
+
 ### Evaluation Metrics
-- Further, we use the `cluster_purity` as a main mertic to evaluate the clsutering performance based on `event_type`. The results can be shown in `Cluster Purity Score`.
+- Further, we use the `cluster_purity` as a main mertic to evaluate the clsutering performance based on `event_type`. The results can be shown in `cluster purity score`.
 - In addition, we use [`cluster_validy_index`](https://hdbscan.readthedocs.io/en/latest/api.html#hdbscan.validity.validity_index) implement in `hdbscan` to compute the density based cluster validity index for the clustering specified by labels. 
 
 Finally, the clustering_demo shows the clusters distributions and ratio of outliers.
 
 ---
+### Clustering Results
+
+```
+Data loaded with shape: (2974716, 41) 
+Number of Clusters:  3
+Clustering Purity Score:  0.9760810779919831
+Clusters Distributions: 
+outliers (-1), 4207
+cluster (0), 114947
+cluster (1), 2855562
+```
+---
 ## Contact
 
 If you have any further questions/suggestions, please contact `hamada.zahera@upb.de`
-
--- main.py
+++ main.py
@@ -2,18 +2,42 @@ import os
 import pandas as pd
 import numpy as np
 import hdbscan
+import argparse
+
 from clustering_evaluation import ClusterPurity
 
-def hdbscan_clustering(cluster_df):
+
+def read_hyperparameters(inputFile):
+    """
+    param: inputFile, path of input file
+    return: a dictionary of hypterparameters of HDBSCAN
+    """ 
+    hyper_param = {}
+    with open(args.config) as input_file:
+        for line in input_file:             
+            (key, val) = line.split()
+            print (key, val)
+            hyper_param[key] = val
+    
+    return hyper_param
+    
+def hdbscan_clustering(cluster_df, param_dict):
     """
     param: Embeddings dataframe to be clustered
     return: a clustered dataframe
-    """
-
+    """ 
     ##-- Cluster the data using HDBSCAN --### -- Consider Hyperparameter tuning later --#
-    clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.3, approx_min_span_tree=True, metric='euclidean',
-     gen_min_span_tree=True, min_cluster_size=10000, min_samples=100, cluster_selection_epsilon= 0.5, 
-                                core_dist_n_jobs=1,allow_single_cluster=False).fit(cluster_df)
+    clusterer = hdbscan.HDBSCAN(
+        algorithm=param_dict['algorithm'], 
+        alpha=float(param_dict['alpha']), 
+        approx_min_span_tree=param_dict['approx_min_span_tree']==1, 
+        metric=param_dict['metric'],
+        gen_min_span_tree=param_dict['gen_min_span_tree']==1,
+        min_cluster_size=int(param_dict['min_cluster_size']),
+        min_samples=int(param_dict['min_samples']),
+        cluster_selection_epsilon= float(param_dict['cluster_selection_epsilon']),
+        core_dist_n_jobs=int(param_dict['core_dist_n_jobs']),
+        allow_single_cluster=param_dict['allow_single_cluster']).fit(cluster_df)
 
     #-- Save clustering results as a new column in the original dataframe --#
     cluster_df['cluster_preds']=clusterer.labels_
@@ -45,34 +69,45 @@ def cluster_distriubions(clustered_df):
     cluster_distributions=clustered_df.groupby('cluster_preds').size()
     for cluster, size in cluster_distributions.items():
         print (cluster, size)
-
         
         
 if __name__ == "__main__":
 
-    input_data='/home/daikiri/DAIKIRI/src/Hamada/merged.csv' # use your own path
-    embedding_data='/home/daikiri/DAIKIRI/src/Hamada/Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv' 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_data", type=str, default='', nargs="?",
+                        help="Path of input data e.g. ai4bd.csv or merged.csv")
+    #input_data='/home/daikiri/DAIKIRI/src/Hamada/merged.csv' # use your own path
+    
+    parser.add_argument("--embedding_data", type=str, default='', nargs="?",
+                        help="full path of embedding data e.g.Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv")
+    #embedding_data='/home/daikiri/DAIKIRI/src/Hamada/Vectograph_Results/2020-08-08 01:00:07.899851/PYKE_50_embd.csv' 
 
+    parser.add_argument("--config", type=str, default='', nargs="?",
+                        help="full path of config file to read hyper-parameter values. e.g. param.txt")
+
+    args = parser.parse_args()
+    
     # -- load the input dataset--#
-    input_df=pd.read_csv(input_data, low_memory=True)
+    input_df=pd.read_csv(args.input_data, low_memory=True)
     input_df.index = 'Event_' + input_df.index.astype(str)
     num_rows, num_cols = input_df.shape  # at max num_rows times num_cols columns.
     column_names = input_df.columns
-    
     print ('input_data (ai4bd) loaded with shape', input_df.shape) 
     
     #-- load the embeddings data --#
-    embedding_input=pd.read_csv(embedding_data, index_col=0, low_memory=True)
+    embedding_input=pd.read_csv(args.embedding_data, index_col=0, low_memory=True)
     #consider only events embedding (Event_id)
     embedding_index=embedding_input.index.tolist()
     prefix = 'Event_'
     event_ids=list(filter(lambda x: x.startswith(prefix), embedding_index))
-    events_df=embedding_input.loc[event_ids]
-    
+    events_df=embedding_input.loc[event_ids]    
     print ('embedding_data loaded with shape', events_df.shape )
 
+    # Read hyper-paramters from file
+    hyper_param=read_hyperparameters(args.config)    
+
     #-- Clustering using HDBSCAN --#
-    clustered_df=hdbscan_clustering(events_df)
+    clustered_df=hdbscan_clustering(events_df, hyper_param)
 
     cluster_preds= events_df['cluster_preds'].tolist()
     cluster_true= input_df['type'].tolist()
@@ -80,9 +115,7 @@ if __name__ == "__main__":
     #-- Evaluate the clustering performance --#
     purity_score=evaluate_ClusteringPurity(cluster_preds, cluster_true)
     print('Clustering Purity Score: ', purity_score)
-    
-    
+        
     # -- Print Cluster Distributions --#
     cluster_distriubions(clustered_df)
-
     
